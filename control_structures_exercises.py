cd0628e5a1e56661fe8a6be8cb020050201d6c8f
randyfrench <randy.a.french@gmail.com>
2021-07-10 23:09:19 -0500

    update on the exercises

--- control_structures_exercises.py
+++ control_structures_exercises.py
@@ -24,7 +24,42 @@ print(f"Total week's paycheck is ${total}")       #how much the week's paycheck 
     # work more than 40 hours
 hrs_worked_in_one_week = 40  #the number of hours worked in one week
 hourly_rate = 40  #the hourly rate
-more_than_40hrs = 5
+more_than_40hrs = 5  # above 40 hours worked
 time_and_half = 60  # the time and a half rate     
-total = hrs_worked_in_one_week * hourly_rate
+total = hrs_worked_in_one_week * hourly_rate + more_than_40hrs * time_and_half
 print(f"Total week's paycheck is ${total}")
+
+#2Loop Basics
+
+    #While
+#i = 5       #Create an integer variable i with a value of 5.
+#while i <= 15:  # Create a while loop that runs so long as i is less than or equal to 15.
+        #print(i)
+        #Each loop iteration, output the current value of i, then increment i by one.
+i = 0
+i = 5
+while i <= 15:
+        print(i)
+        i = i + 1
+    #Your output should look like this:
+
+#5,6,7,8,9,10,11,12,13,14,15
+
+#Create a while loop that will count by 2's starting with 0 and ending at 100. Follow each number with 
+#a new line.
+x = 0
+x = 2
+while x <= 100:
+        print(x)
+        x = x + 2
+
+#Alter your loop to count backwards by 5's from 100 to -10.
+x = 100
+while x > -15:
+        print(x)
+        x = x - 5
+
+
+
+#Create a while loop that starts at 2, and displays the number squared on each line while the number is
+ #less than 1,000,000. Output should equal:
