22c4900b8c2fb0f819585f9b3154072adf2a91c3
randyfrench <randy.a.french@gmail.com>
2021-07-13 10:55:31 -0500

    more exercises completed

--- function_exercises.py
+++ function_exercises.py
@@ -9,6 +9,7 @@ is_two(2)
 
     # 2. Define a function named is_vowel. It should return True if the passed string is a vowel, 
     # False otherwise.ef is_vowel(string):
+def is_vowel(string):   
     vowles = "AEIOUaeiou"
     for x in string:
         if x in string[0] in vowles:
@@ -32,16 +33,32 @@ is_consonant(B)
 
     # 4. Define a function that accepts a string that is a word. The function should capitalize the 
     # first letter of the word if the word starts with a consonant.
+def is_consonant(string):
+    consonant = "bcdfghjklmnpqrstvwxyz"
+    for x in string:
+        if x in string[0] in consonant.upper():
+    return consonant
+is_consonant("potlicker")
+
     # 5. Define a function named calculate_tip. It should accept a tip percentage (a number between 0 
     # and 1) and the bill total, and return the amount to tip.
+def calculate_tip(x):
+
     # 6. Define a function named apply_discount. It should accept a original price, and a discount 
     # percentage, and return the price after the discount is applied.
+def apply_discount(x):
+
     # 7. Define a function named handle_commas. It should accept a string that is a number that 
     # contains commas in it as input, and return a number as output.
+def handle_commas(x):
+
     # 8. Define a function named get_letter_grade. It should accept a number and return the letter 
     # grade associated with that number (A-F).
+def get_letter_grade(x):
     # 9. Define a function named remove_vowels that accepts a string and returns a string with all 
     # the vowels removed.
+def remove_vowels(x):
+
     # 10. Define a function named normalize_name. It should accept a string and return a valid python 
     # identifier, that is:
     #     anything that is not a valid python identifier should be removed
@@ -52,6 +69,10 @@ is_consonant(B)
     #         Name will become name
     #         First Name will become first_name
     #         % Completed will become completed
-    # 11. Write a function named cumulative_sum that accepts a list of numbers and returns a list that is the cumulative sum of the numbers in the list.
+def normalize_name(x):
+
+    # 11. Write a function named cumulative_sum that accepts a list of numbers and returns a list 
+    # that is the cumulative sum of the numbers in the list.
     #     cumulative_sum([1, 1, 1]) returns [1, 2, 3]
     #     cumulative_sum([1, 2, 3, 4]) returns [1, 3, 6, 10]
+def cumulative_sum(x):
