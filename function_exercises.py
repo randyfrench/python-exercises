628153cea3736548821312a87e478635db31413b
randyfrench <randy.a.french@gmail.com>
2021-07-13 16:49:35 -0500

    more practice

--- function_exercises.py
+++ function_exercises.py
@@ -1,7 +1,7 @@
 # 1. Define a function named is_two. It should accept one input and return True if the passed 
     # input is either the number or the string 2, False otherwise.
 def is_two(x):
-    if x == 2:
+    if x == 2 or x == '2':
         return True
     else:
         return False
@@ -21,43 +21,43 @@ is_vowel('U')
 
     # 3. Define a function named is_consonant. It should return True if the passed string is a 
     # consonant, False otherwise. Use your is_vowel function to accomplish this.
-def is_consonant(string):
-    consonant = "bcdfghjklmnpqrstvwxyz"
-    for x in string:
-        if x in string[0] in consonant:
-            return True
-        else:
-            return False
-    return consonant
-is_consonant(B)    
+# def is_consonant(string):
+#     consonant = "bcdfghjklmnpqrstvwxyz"
+#     for x in string:
+#         if x in string[0] in consonant:
+#             return True
+#         else:
+#             return False
+#     return consonant
+# is_consonant("B")    
 
     # 4. Define a function that accepts a string that is a word. The function should capitalize the 
     # first letter of the word if the word starts with a consonant.
-def is_consonant(string):
-    consonant = "bcdfghjklmnpqrstvwxyz"
-    for x in string:
-        if x in string[0] in consonant.upper():
-    return consonant
-is_consonant("potlicker")
+# def is_consonant(string):
+#     consonant = "bcdfghjklmnpqrstvwxyz"
+#     for x in string:
+#         if x in string[0] in consonant.capitalize():
+#     return consonant
+# is_consonant("potlicker")
 
     # 5. Define a function named calculate_tip. It should accept a tip percentage (a number between 0 
     # and 1) and the bill total, and return the amount to tip.
-def calculate_tip(x):
+#def calculate_tip(x):
 
     # 6. Define a function named apply_discount. It should accept a original price, and a discount 
     # percentage, and return the price after the discount is applied.
-def apply_discount(x):
+#def apply_discount(x):
 
     # 7. Define a function named handle_commas. It should accept a string that is a number that 
     # contains commas in it as input, and return a number as output.
-def handle_commas(x):
+#def handle_commas(x):
 
     # 8. Define a function named get_letter_grade. It should accept a number and return the letter 
     # grade associated with that number (A-F).
-def get_letter_grade(x):
+#def get_letter_grade(x):
     # 9. Define a function named remove_vowels that accepts a string and returns a string with all 
     # the vowels removed.
-def remove_vowels(x):
+#def remove_vowels(x):
 
     # 10. Define a function named normalize_name. It should accept a string and return a valid python 
     # identifier, that is:
@@ -69,10 +69,10 @@ def remove_vowels(x):
     #         Name will become name
     #         First Name will become first_name
     #         % Completed will become completed
-def normalize_name(x):
+#def normalize_name(x):
 
     # 11. Write a function named cumulative_sum that accepts a list of numbers and returns a list 
     # that is the cumulative sum of the numbers in the list.
     #     cumulative_sum([1, 1, 1]) returns [1, 2, 3]
     #     cumulative_sum([1, 2, 3, 4]) returns [1, 3, 6, 10]
-def cumulative_sum(x):
+#def cumulative_sum(x):
