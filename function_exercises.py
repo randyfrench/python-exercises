de6e5694bd9b2f9a21956fc13f0e08299c12889a
randyfrench <randy.a.french@gmail.com>
2021-07-20 08:53:45 -0500

    more exercises completed

--- function_exercises.py
+++ function_exercises.py
@@ -31,17 +31,17 @@ def is_consonant(string):
     return consonant
 is_consonant("B")    
 
-    # 4. Define a function that accepts a string that is a word. The function should capitalize the 
-    # first letter of the word if the word starts with a consonant.
-# def is_consonant(string):
-#     consonant = "bcdfghjklmnpqrstvwxyz"
-#     for x in string:
-#         if x in string[0] in consonant.capitalize():
-#     return consonant
-# is_consonant("potlicker")
+# 4. Define a function that accepts a string that is a word. The function should capitalize the 
+# first letter of the word if the word starts with a consonant.
 
-    # 5. Define a function named calculate_tip. It should accept a tip percentage (a number between 0 
-    # and 1) and the bill total, and return the amount to tip.
+def is_consonant(string):
+    if string[0].lower() not in 'aeiou':
+        return string.capitalize()
+    return string
+
+# 5. Define a function named calculate_tip. It should accept a tip percentage (a number between 0 
+# and 1) and the bill total, and return the amount to tip.
+
 def calculate_tip(bill, tip_percent):
     
     return bill * tip_percent
@@ -51,8 +51,9 @@ tip_percent = .20
 
 calculate_tip(bill, tip_percent)
 
-    # 6. Define a function named apply_discount. It should accept a original price, and a discount 
-    # percentage, and return the price after the discount is applied.
+# 6. Define a function named apply_discount. It should accept a original price, and a discount 
+# percentage, and return the price after the discount is applied.
+
 def apply_discount(price, discount_percent):
     
     return price - (price * discount_percent / 100)
@@ -64,8 +65,9 @@ apply_discount(price, discount_percent)
     
 
 
-    # 7. Define a function named handle_commas. It should accept a string that is a number that 
-    # contains commas in it as input, and return a number as output.
+# 7. Define a function named handle_commas. It should accept a string that is a number that 
+# contains commas in it as input, and return a number as output.
+
 def handle_commas(string):
     number = ""
     for char in string:
@@ -76,25 +78,49 @@ def handle_commas(string):
 string = "2,0,1,25.5,42,0,0,10"
 handle_commas(string)
 
-    # 8. Define a function named get_letter_grade. It should accept a number and return the letter 
-    # grade associated with that number (A-F).
-#def get_letter_grade(x):
+# 8. Define a function named get_letter_grade. It should accept a number and return the letter 
+# grade associated with that number (A-F).
 
+def get_letter_grade(number):
+    if number <= 69:        
+        grade = "F"
+    elif number <= 79:
+        grade = "C"
+    elif number <= 89:
+        grade = "B"
+    else:
+        grade = "A"
+    
+    return grade
 
-  # 9. Define a function named remove_vowels that accepts a string and returns a string with all 
-    # the vowels removed.
-#def remove_vowels(x):
+number = "88"
+get_letter_grade(number)
 
-    # 10. Define a function named normalize_name. It should accept a string and return a valid python 
-    # identifier, that is:
-    #     anything that is not a valid python identifier should be removed
-    #     leading and trailing whitespace should be removed
-    #     everything should be lowercase
-    #     spaces should be replaced with underscores
-    #     for example: 
-    #         Name will become name
-    #         First Name will become first_name
-    #         % Completed will become completed
+# 9. Define a function named remove_vowels that accepts a string and returns a string with all 
+# the vowels removed.
+def remove_vowels(string):
+    no_vowels = " "
+    
+    for char in string:
+        if char.lower() not in "aeiou":
+            no_vowels += char
+            
+    return no_vowels
+
+string = "This string will have no vowels in it when hit"
+remove_vowels(string)
+
+# 10. Define a function named normalize_name. It should accept a string and return a valid python 
+# identifier, that is:
+#     anything that is not a valid python identifier should be removed
+#     leading and trailing whitespace should be removed
+#     everything should be lowercase
+#     spaces should be replaced with underscores
+#     for example: 
+#         Name will become name
+#         First Name will become first_name
+#         % Completed will become completed
+
 #def normalize_name(x):
 
     # 11. Write a function named cumulative_sum that accepts a list of numbers and returns a list 
